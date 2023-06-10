c82da7a43e0d9a0986aca8841a3a8673ca15f1ae
miso-devel <kengo279@icloud.com>
2023-06-10 16:41:38 +0900

    自分の環境で試してみる

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { MESSAGE_TEXT, NOTICE_TEXT, START_TEXT } from "./constants.ts";
-import { createBot, Cron, Intents, startBot } from "./deps.ts";
+import { ApplicationCommandPermissionTypes, createBot, Cron, Intents, startBot } from "./deps.ts";
 import { Secret } from "./secret.ts";
 import { createResultMessage, getReactedMessage, getReactingUserIds, getReactionEmojis } from "./utils.ts";
 
@@ -18,35 +18,39 @@ bot.events.messageCreate = (b, message) => {
     b.helpers.sendMessage(message.channelId, {
       content: "ヘルプ！",
     });
+    console.log("help console");
   }
 };
 
 // 指定時刻に1on1への参加を可否を聞くCron
 // 月曜日の21時に流す
-new Cron("40 16 * * 6", () => {
+new Cron("45 16 * * 6", () => {
   bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: MESSAGE_TEXT });
+  console.log("message console");
 });
 
 // 開始時刻前になったらマッチ結果をお知らせしてくれるCron
 // 木曜日の21時に流す
-new Cron("42 16 * * 6", async () => {
+new Cron("47 16 * * 6", async () => {
   const messages = await bot.helpers.getMessages(Secret.MY_CHANNEL_ID);
   const reactedMessage = getReactedMessage({ messages });
   const reactedEmojis = getReactionEmojis(reactedMessage);
   const reactingUserIds = await getReactingUserIds({ bot, messageId: reactedMessage?.id, emojis: reactedEmojis });
   const resultMessage = createResultMessage(reactingUserIds);
   bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: NOTICE_TEXT + resultMessage });
+  console.log("match console");
 });
 
 // 開催時刻に流れる
 // 木曜日の21時30分に流す
-new Cron("43 16 * * 6", async () => {
+new Cron("48 16 * * 6", async () => {
   const messages = await bot.helpers.getMessages(Secret.MY_CHANNEL_ID);
   const reactedMessage = getReactedMessage({ messages });
   const reactedEmojis = getReactionEmojis(reactedMessage);
   const reactingUserIds = await getReactingUserIds({ bot, messageId: reactedMessage?.id, emojis: reactedEmojis });
   const resultMessage = createResultMessage(reactingUserIds);
   bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: START_TEXT + resultMessage });
+  console.log("announce console");
 });
 
 await startBot(bot);
