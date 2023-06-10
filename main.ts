f7fb8e0e7813add4587bfda458e99c95697d3804
miso-devel <kengo279@icloud.com>
2023-06-10 18:34:19 +0900

    完成

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import { MESSAGE_TEXT, NOTICE_TEXT, START_TEXT } from "./constants.ts";
 import { createBot, Cron, Intents, startBot } from "./deps.ts";
 import { Secret } from "./secret.ts";
-import { createResultMessage, getReactedMessage, getReactingUserIds, getReactionEmojis } from "./utils.ts";
+import {
+  createResultMessage,
+  createStartMessage,
+  getReactedMessage,
+  getReactingUserIds,
+  getReactionEmojis,
+} from "./utils.ts";
 
 const bot = createBot({
   token: Secret.DISCORD_TOKEN,
@@ -14,15 +20,6 @@ const bot = createBot({
 });
 
 const day = 60 * 60 * 24;
-
-bot.events.messageCreate = (b, message) => {
-  if (message.content === "!help") {
-    b.helpers.sendMessage(message.channelId, {
-      content: "ヘルプ！100",
-    });
-    console.log("help console");
-  }
-};
 
 // 指定時刻に1on1への参加を可否を聞くCron
 // 月曜日の21時に流す
@@ -39,8 +36,7 @@ new Cron("* 46 17 * * SAT", { interval: day * 7, timezone: "Asia/Tokyo" }, async
   const reactedEmojis = getReactionEmojis(reactedMessage);
   const reactingUserIds = await getReactingUserIds({ bot, messageId: reactedMessage?.id, emojis: reactedEmojis });
   const resultMessage = createResultMessage(reactingUserIds);
-  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: NOTICE_TEXT + resultMessage });
-  console.log("match console");
+  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: resultMessage });
 });
 
 // 開催時刻に流れる
@@ -50,9 +46,9 @@ new Cron("* 47 17 * * SAT", { interval: day * 7, timezone: "Asia/Tokyo" }, async
   const reactedMessage = getReactedMessage({ messages });
   const reactedEmojis = getReactionEmojis(reactedMessage);
   const reactingUserIds = await getReactingUserIds({ bot, messageId: reactedMessage?.id, emojis: reactedEmojis });
-  const resultMessage = createResultMessage(reactingUserIds);
-  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: START_TEXT + resultMessage });
-  console.log("announce console");
+  if (reactingUserIds.length === 0 || reactingUserIds.length === 1) return;
+  const startMessage = createStartMessage(reactingUserIds);
+  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: startMessage });
 });
 
 await startBot(bot);
--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-import { MESSAGE_TEXT, NO_MATCH_TEXT } from "./constants.ts";
+import { MESSAGE_TEXT, NO_MATCH_TEXT, NOTICE_TEXT, START_TEXT } from "./constants.ts";
 import { Bot, Collection, Message } from "./deps.ts";
 import { Secret } from "./secret.ts";
 
@@ -16,24 +16,30 @@ export const getReactionEmojis = (message: Message | undefined) => {
   return reactionEmojis;
 };
 
-// reactionをしているユーザーのIDをリアクションごとに返す
+// reactionをしているユーザーのIDを重複を削除してから返す
 type TgetReactingUserIdsArg = { bot: Bot; messageId: bigint | undefined; emojis: (string | undefined)[] };
 export const getReactingUserIds = async ({ bot, messageId, emojis }: TgetReactingUserIdsArg) => {
   if (!messageId) return [];
+
   const filterUndefinedEmojis = emojis.filter((emoji) => emoji !== undefined) as string[];
+
   const userIds = await Promise.all(filterUndefinedEmojis.map(async (emoji) => {
     const users = await bot.helpers.getReactions(Secret.MY_CHANNEL_ID, messageId, emoji);
     return users.map((user) => user.id);
   }));
-  return userIds;
+
+  const dedupeUserIds = [...new Set(userIds.flat())];
+
+  return dedupeUserIds;
 };
 
 // マッチの結果をメッセージにして返す
-export const createResultMessage = (userIds: bigint[][]) => {
-  const DedupeUserIds = [...new Set(userIds.flat()), 1, 2, 3, 4];
-  // 重複削除の結果0か1の時は1on1をなしにする
-  if (DedupeUserIds.length === 0 || DedupeUserIds.length === 1) return NO_MATCH_TEXT;
-  const randomUserIds = DedupeUserIds.slice().sort(() => Math.random() - Math.random());
+export const createResultMessage = (userIds: bigint[]) => {
+  if (userIds.length === 0 || userIds.length === 1) return NO_MATCH_TEXT;
+
+  // ランダムに並べ替える
+  const randomUserIds = userIds.slice().sort(() => Math.random() - Math.random());
+
   const userIdCount = Math.floor(randomUserIds.length / 2);
   const matchIds = Array.from(
     { length: userIdCount },
@@ -48,5 +54,10 @@ export const createResultMessage = (userIds: bigint[][]) => {
     `<@${matchId[0]}> on <@${matchId[1]}> ${matchId[2] ? `on <@${matchId[2]}>` : ""}`
   ).join("\n");
 
-  return matchText;
+  return NOTICE_TEXT + matchText;
+};
+
+export const createStartMessage = (userIds: bigint[]) => {
+  const joinMembersText = userIds.map((userId) => `<@${userId}>`).join(" ");
+  return START_TEXT + joinMembersText;
 };
