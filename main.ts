657568fa4073e01dca5d90db2eddba5a5a125d06
miso-devel <kengo279@icloud.com>
2023-06-10 18:50:07 +0900

    テスト

--- main.ts
+++ main.ts
@@ -23,13 +23,13 @@ const day = 60 * 60 * 24;
 
 // 指定時刻に1on1への参加を可否を聞くCron
 // 月曜日の21時に流す
-new Cron("10 48 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, () => {
+new Cron("0 52 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, () => {
   bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: MESSAGE_TEXT });
 });
 
 // 開始時刻前になったらマッチ結果をお知らせしてくれるCron
 // 木曜日の21時に流す
-new Cron("30 48 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, async () => {
+new Cron("0 53 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, async () => {
   const messages = await bot.helpers.getMessages(Secret.MY_CHANNEL_ID);
   const reactedMessage = getReactedMessage({ messages });
   const reactedEmojis = getReactionEmojis(reactedMessage);
@@ -40,7 +40,7 @@ new Cron("30 48 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, async 
 
 // 開催時刻に流れる
 // 木曜日の21時30分に流す
-new Cron("50 48 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, async () => {
+new Cron("30 54 18 * * *", { interval: day * 7, timezone: "Asia/Tokyo" }, async () => {
   const messages = await bot.helpers.getMessages(Secret.MY_CHANNEL_ID);
   const reactedMessage = getReactedMessage({ messages });
   const reactedEmojis = getReactionEmojis(reactedMessage);
