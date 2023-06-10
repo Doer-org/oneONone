f5af23051e93aa9723a112a389b305da0bd0224a
miso-devel <kengo279@icloud.com>
2023-06-10 16:57:08 +0900

    自分の環境で試してみる

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { MESSAGE_TEXT, NOTICE_TEXT, START_TEXT } from "./constants.ts";
-import { ApplicationCommandPermissionTypes, createBot, Cron, Intents, startBot } from "./deps.ts";
+import { createBot, Cron, Intents, startBot } from "./deps.ts";
 import { Secret } from "./secret.ts";
 import { createResultMessage, getReactedMessage, getReactingUserIds, getReactionEmojis } from "./utils.ts";
 
@@ -21,6 +21,16 @@ bot.events.messageCreate = (b, message) => {
     console.log("help console");
   }
 };
+
+new Cron("45 16 * * */6", () => {
+  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: MESSAGE_TEXT });
+  console.log("message console-1");
+});
+
+new Cron("0 17 1 * SAT", () => {
+  bot.helpers.sendMessage(Secret.MY_CHANNEL_ID, { content: MESSAGE_TEXT });
+  console.log("message console-2");
+});
 
 // 指定時刻に1on1への参加を可否を聞くCron
 // 月曜日の21時に流す
